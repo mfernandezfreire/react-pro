f043dd828d42bed637423c31a4f4930b8f9db370
mfernandezfreire <mfernandezfreir.ext@acciona.com>
2022-09-29 09:35:16 +0200

    Component Control Props alternative done

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -10,13 +10,7 @@ interface useProductArgs {
 export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => {
     const [counter, setCounter] = useState(value);
 
-    const isControlled = useRef(!!onChange);
-
     const increaseBy = (value: number) => {
-
-        if (isControlled.current)  {
-            return onChange!({ count: value, product });
-        }
 
         const newValue = Math.max(counter + value, 0)
         setCounter(newValue);
